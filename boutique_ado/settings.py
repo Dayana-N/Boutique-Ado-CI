b0941bca79e71420565abe15edf5e1d805e7ea13
Dayana <dayana.nashkova@yahoo.com>
2023-09-16 13:38:11 +0100

    feat: add checkout template

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -52,6 +52,9 @@ INSTALLED_APPS = [
     'products.apps.ProductsConfig',
     'bag.apps.BagConfig',
     'checkout.apps.CheckoutConfig',
+    # others
+    'crispy_forms',
+
 ]
 
 MIDDLEWARE = [
@@ -66,6 +69,8 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'boutique_ado.urls'
 
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -80,8 +85,13 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
                 'bag.contexts.bag_contents',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
