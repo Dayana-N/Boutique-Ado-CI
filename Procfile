1594248bcc37764e8a983f8b802ada24a72498cb
Dayana <dayana.nashkova@yahoo.com>
2023-09-21 10:58:17 +0100

    prepare for deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: unicorn boutique_ado.wsgi:application
--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 import os
 
+import dj_database_url
+
 if os.path.exists("env.py"):
     import env
 
@@ -30,7 +32,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ci-boutique-465a4012e8f4.herokuapp.com/', 'localhost']
 
 
 # Application definition
@@ -124,13 +126,18 @@ WSGI_APPLICATION = 'boutique_ado.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
